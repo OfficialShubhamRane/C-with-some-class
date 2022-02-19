e29603e69f83ed97df1beabc3c889c8ff74b7d02
unknown <official.shubhamrane@gmail.com>
2022-02-19 15:20:38 -0600

    Pizza slices

--- Basic_concepts/Test/main.cpp
+++ Basic_concepts/Test/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello World!";
-    return 0;
+    std::cout << "Welcome to JAPP!" << "\n";
+
+    int pizza_area = 500;
+    int required_slices = 1;
+
+    std::cout << "How many slices are required? \n";
+    std::cin >> required_slices;
+
+    int slice_area = pizza_area / required_slices ;
+
+    std::cout << "Each slice has area of " << slice_area << " units."<< "\n";
+    printf("Each slice has area of [%i] units.", slice_area); // most stuuf from c will work inside of c++.
+
 }
